07cfca0ad1fc363d559c5e884563d97ca97f984d
Ved5412 <study.vedp@gmail.com>
2025-04-21 11:58:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-//* Target Date Must be Like Below
-//var date = new Date("June 15, 2016 12:45:00");
+var clock;
+var isPaused = false;
 
-// Just for running purpose
-//=======================================
-var start = new Date();
-start.setDate(start.getDate());
-start.setHours(0,0,0,0)
-//======================================
-var now   = new Date();
-
-var diff = (now.getTime() - start.getTime()) / 1000;
-
-	var clock = $('#clock1').FlipClock(diff, {
+$(document).ready(function () {
+	// Initialize FlipClock with 0
+	clock = $('#clock1').FlipClock(0, {
 		clockFace: 'HourlyCounter',
-		countdown: false,
-		showSeconds: true
+		countdown: true,
+		autoStart: false,
+		callbacks: {
+			stop: function () {
+				if (!isPaused) {
+					alert("Time's up!");
+				}
+			}
+		}
 	});
+
+	// Start Button
+	$('#startBtn').click(function () {
+		let minutes = parseInt($('#minutes').val()) || 0;
+		let seconds = parseInt($('#seconds').val()) || 0;
+		let totalSeconds = (minutes * 60) + seconds;
+
+		if (totalSeconds > 0) {
+			clock.setTime(totalSeconds);
+			clock.setCountdown(true);
+			isPaused = false;
+			clock.start();
+		} else {
+			alert("Please enter a valid time.");
+		}
+	});
+
+	// Pause Button
+	$('#pauseBtn').click(function () {
+		if (clock.running) {
+			clock.stop();
+			isPaused = true;
+		} else if (clock.getTime().time > 0) {
+			isPaused = false;
+			clock.start();
+		}
+	});
+
+	// Reset Button
+	$('#resetBtn').click(function () {
+		clock.stop();
+		clock.setTime(0);
+		isPaused = false;
+	});
+
+	// Theme Toggle
+	$('#themeSwitch').on('change', function () {
+		$('body').toggleClass('dark');
+	});
+});
